5da3217c23c2243b514abebd6d8aab9005ff8dc8
Giovanni Gurrali <giovannigurrali@MacBook-Air-di-Giovanni.local>
2024-09-27 10:52:31 +0200

    Terzo commit

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,25 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
+import { aws_dynamodb } from "aws-cdk-lib";
 
-defineBackend({
+export const backend = defineBackend({
   auth,
   data,
 });
+
+
+const externalDataSourcesStack = backend.createStack("MyExternalDataSources");
+
+
+const externalTable = aws_dynamodb.Table.fromTableName(
+    externalDataSourcesStack,
+    "MyExternalRaspberryPi_data_table",
+    "raspberryPi_data_table"
+);
+
+
+backend.data.addDynamoDbDataSource(
+    "ExternalRaspberryPi_data_tableSource",
+    externalTable
+);
