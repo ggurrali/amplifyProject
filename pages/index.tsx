ea382a7d5187d8620f947149ba7269309f4314f3
Giovanni Gurrali <giovannigurrali@MacBook-Air-di-Giovanni.local>
2024-09-30 09:18:09 +0200

    Modifica id table (secondo tentativo)

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,27 +5,6 @@ import type { Schema } from "@/amplify/data/resource";
 const client = generateClient<Schema>();
 
 export default async function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  const {data, errors} = await client.mutations.deleteRaspberryPi_data_table({
-    id: "<post-id>"
-  })
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
 
   return (
       <main>
